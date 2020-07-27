4cc7dffdea9a836a74ae690ca7a26984fe2be466
isan0880 <ikhsanaditya33@gmail.com>
2020-07-27 13:02:25 +0700

    1900018221

--- open.php
+++ open.php
@@ -7,9 +7,6 @@ require 'koneksi.php';
 <head>
   <title>Gallery</title>
   <link rel="icon" href="images/isan.png">
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
